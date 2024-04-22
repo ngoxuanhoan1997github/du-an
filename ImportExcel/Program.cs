e1c468eec42421fc8b53b27a371186a3bca40071
ngoxuanhoan1997github <hoanngo1997@gmail.com>
2024-04-22 10:44:00 +0700

    thay doi

--- ImportExcel/Program.cs
+++ ImportExcel/Program.cs
@@ -16,7 +16,7 @@ namespace ImportExcel
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new DangNhap());
+            Application.Run(new Import());
         }
     }
 }
